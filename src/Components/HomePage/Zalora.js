9b397867ceac0031ecc71863ad43f4312a4ec91e
ayiangio <moh.harisangio@gmail.com>
2020-01-01 17:56:41 +0700

    rion testing

--- src/Components/HomePage/Zalora.js
+++ src/Components/HomePage/Zalora.js
@@ -20,7 +20,7 @@ class headZalora extends Component {
                         fontWeight: 'lighter',float:'left'
 					}}
 				>
-					Rion Ming
+					Rion Miing
 				</a>
 				<a
 					href="/women"
