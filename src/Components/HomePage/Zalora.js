bf7da5971ce1fe97281789da163693cde84b853d
Ayiangio <moh.harisangio@gmail.com>
2020-10-28 13:40:05 +0700

    add

--- src/Components/HomePage/Zalora.js
+++ src/Components/HomePage/Zalora.js
@@ -20,7 +20,7 @@ class headZalora extends Component {
                         fontWeight: 'lighter',float:'left'
 					}}
 				>
-					Zalora KW Using k8s
+					Hello from k8s
 				</a>
 				<a
 					href="/women"
